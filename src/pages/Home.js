4b6a33e71e007701b77f939ed299106895b7faf8
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-10-03 18:54:58 -0300

    campo de busca tela home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 export default function Home(){
     return (
-        <View>
-            <Text>HOME</Text>
-        </View>
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{backgroundColor: '#FFF' }}
+        >
+            <View style={styles.header}>
+                <View style={styles.inputArea}>
+                    <Feather name='search' size={24} color="black"/>
+                    <TextInput
+                        placeholder="O que está procurando?"
+                        style={styles.input}
+                    />
+                </View>
+            </View>
+        
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    header:{
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        marginVertical: 20,
+    },
+    inputArea:{
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '98%',
+        backgroundColor: '#FFF',
+        elevation: 2,
+        paddingHorizontal: 10,
+        height: 37,
+        borderRadius: 10,
+    },
+    input:{
+        fontFamily: 'Montserrat_500Medium',
+        paddingHorizontal: 10,
+        fontSize: 13,
+        width: '90%'
+    }
+
+});
