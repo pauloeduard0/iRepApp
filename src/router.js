6de98f36b943ef3d8d19b8d83989c2f08d8cd9c5
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2021-11-06 00:47:51 -0300

    Rotas das páginas de cadastros e login

--- src/router.js
+++ src/router.js
@@ -6,6 +6,9 @@ import {TouchableOpacity} from 'react-native';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
+import Login from './pages/Login';
+import StudentRegistration from './pages/StudentRegistration';
+import RepRegistration from './pages/RepRegistration';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +16,66 @@ function Routes(){
     return(
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen
+                    name="login"
+                    component={Login} 
+                    options={{
+                        title:'Bem vindo',
+                        headerTitleStyle:{
+                            fontFamily:'Montserrat_700Bold',
+                        },
+                           
+                        headerRight: () => (
+                            <TouchableOpacity style={{ marginRight: 15 }}>
+                                <Feather
+                                    name="shopping-bag"
+                                    size={24}
+                                    color="black"
+                                />
+                            </TouchableOpacity>
+                        )
+                    }} 
+                />
+                <Stack.Screen
+                    name="cadastro"
+                    component={StudentRegistration}
+                    options={{
+                        title:'Cadastro',
+                        headerTitleStyle:{
+                            fontFamily:'Montserrat_700Bold',
+                        },
+                           
+                        headerRight: () => (
+                            <TouchableOpacity style={{ marginRight: 15 }}>
+                                <Feather
+                                    name="shopping-bag"
+                                    size={24}
+                                    color="black"
+                                />
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+                <Stack.Screen
+                    name="cadastrorep"
+                    component={RepRegistration}
+                    options={{
+                        title:'Cadastro Rep :)',
+                        headerTitleStyle:{
+                            fontFamily:'Montserrat_700Bold',
+                        },
+                           
+                        headerRight: () => (
+                            <TouchableOpacity style={{ marginRight: 15 }}>
+                                <Feather
+                                    name="shopping-bag"
+                                    size={24}
+                                    color="black"
+                                />
+                            </TouchableOpacity>
+                        )
+                    }}  
+                />
                 <Stack.Screen
                     name="home"
                     component={Home}
