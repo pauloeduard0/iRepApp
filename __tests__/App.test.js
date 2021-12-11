66926f4608254b8d5e25822136dab684c50f0ca4
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2021-12-11 10:24:41 -0300

    correção nome variavel

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -4,6 +4,6 @@ import renderer from 'react-test-renderer';
 import App from '../App';
 
 it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const rend = renderer.create(<App />).toJSON();
+    expect(rend).toMatchSnapshot();
+})
